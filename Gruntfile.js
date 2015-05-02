131c121133d393b29f83a450ace11b7eb03ec9fa
Brad Dougherty <me@brad.is>
2015-05-02 13:52:09 -0400

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,59 @@
-/*jshint node:true */
+/* eslint-env node */
 module.exports = function(grunt) {
 	'use strict';
+
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt);
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		jshint: {
-			options: grunt.file.readJSON('.jshintrc'),
-			dev: {
-				src: ['Gruntfile.js', 'src/**/*.js']
-			},
-			beforeconcat: {
-				options: {
-					devel: false
-				},
-				src: ['Gruntfile.js', 'src/**/*.js']
-			},
-			afterconcat: {
-				options: {
-					devel: false
-				},
-				src: ['bigscreen.js']
-			}
-		},
-
-		concat: {
-			options: {
-				stripBanners: true,
-				banner: '/*! <%= pkg.name %>\n * v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n * <%= pkg.homepage %>\n * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n */\n'
-			},
-			all: {
-				src: ['src/**/*.js'],
-				dest: 'bigscreen.js'
+		eslint: {
+			src: {
+				src: [
+					'Gruntfile.js',
+					'src/bigscreen.js'
+				]
 			}
 		},
 
 		uglify: {
-			options: {
-				banner: '// <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> - <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n'
-			},
 			unminified: {
 				options: {
-					banner: '',
+					banner: '/*! <%= pkg.name %>\n * v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n * <%= pkg.homepage %>\n * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n */\n',
 					mangle: false,
 					compress: false,
 					preserveComments: 'some',
 					beautify: true
 				},
-				src: ['bigscreen.js'],
-				dest: 'bigscreen.js'
+				files: {
+					'bigscreen.js': ['src/bigscreen.js']
+				}
 			},
 			minified: {
-				src: ['bigscreen.js'],
-				dest: 'bigscreen.min.js'
+				options: {
+					banner: '// <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> - <%= _.pluck(pkg.licenses, "type").join(", ") %> License',
+					sourceMap: true
+				},
+				files: {
+					'bigscreen.min.js': ['src/bigscreen.js']
+				}
 			}
 		}
 
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.registerTask('remove_map_comment', function() {
+		var fs = require('fs');
+		var files = Object.keys(grunt.config('uglify.minified.files'));
 
-	grunt.registerTask('default', ['jshint:dev']);
-	grunt.registerTask('release', ['jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'uglify:unminified', 'uglify:minified']);
+		files.forEach(function(file) {
+			var text = fs.readFileSync(file, 'utf8').replace(/\/\/# sourceMappingURL=\S+/, '');
+			fs.writeFileSync(file, text);
+		});
+	});
+
+	grunt.registerTask('default', ['eslint']);
+	grunt.registerTask('build', ['eslint', 'uglify', 'remove_map_comment']);
 
 };
